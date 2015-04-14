a4220ee504058efea513742a24535f098b063d7a
Alpha2546 <adriaanvanloon@gmail.com>
2015-04-15 01:46:48 +0200

    Map pack update

---
+++ ui/mods/alpha2546/maplist.js
@@ -0,0 +1,13 @@
+cShareSystems.load_pas("Alpha2546 maps", [
+	"coui://ui/mods/alpha2546/systems/diona.pas",
+	"coui://ui/mods/alpha2546/systems/hydrogenix_facility_f407_1v1.pas",
+	"coui://ui/mods/alpha2546/systems/hydrogenix_facility_f407_3v3.pas",
+	"coui://ui/mods/alpha2546/systems/ice_madness.pas",
+	"coui://ui/mods/alpha2546/systems/lava_carnage.pas",
+	"coui://ui/mods/alpha2546/systems/lazona.pas",
+	"coui://ui/mods/alpha2546/systems/reminisce.pas",
+	"coui://ui/mods/alpha2546/systems/the_reef.pas",
+	"coui://ui/mods/alpha2546/systems/the_t3mple_3v3.pas",
+	"coui://ui/mods/alpha2546/systems/the_t3mple_v2.pas",
+	"coui://ui/mods/alpha2546/systems/zetox.pas"
+]);
